3e97bb874a86c4476f894353be356b3ed8ee1032
bharath <bharathkumar.feb14@gmail.com>
2019-10-28 21:41:55 +0530

    insert script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,9 @@
 INSERT INTO ROLE VALUES (1,1, CURRENT_DATE(),1, CURRENT_DATE(),CURRENT_TIMESTAMP,'Client','Active'); 
+
+
+INSERT INTO USER( user_id, first_name, mobile, email_id , gender, marital_status, gstp_number, status, created_by, created_date)  VALUES 
+ (1,'bharath','9789944159','bharathkumar.feb14@gmail.com','Male', 'Single', '1345','InActive', 1, CURRENT_DATE());
+ 
+ INSERT INTO USER_ROLE ( user_role_id,created_by,created_date,status,role_id,user_id ) VALUES
+ (1,1, CURRENT_DATE(),'Active',1,1);
+ 
