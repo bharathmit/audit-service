013261ae945cba151a036414e8f18e56b0a592df
bharath <bharathkumar.feb14@gmail.com>
2019-11-15 13:21:50 +0530

    rest controller

--- src/main/java/com/audit/app/controller/PaymentController.java
+++ src/main/java/com/audit/app/controller/PaymentController.java
@@ -8,9 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.audit.app.exception.BusinessException;
@@ -18,7 +18,7 @@ import com.audit.app.exception.response.ErrorDescription;
 import com.audit.app.utils.PaytmDetails;
 import com.paytm.pg.merchant.CheckSumServiceHelper;
 
-@Controller
+@RestController("/payment")
 public class PaymentController {
 
 	@Autowired
